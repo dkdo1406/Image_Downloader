2c7f6603aa4712a72bdd811c18bff25a88be3b4a
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2023-03-02 11:23:41 +0900

    setting

---
+++ Image_Downloader/Image_Downloader/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Image_Downloader
+//
+//  Created by Hyeongjung on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+    
+    
+    
+    
+
+
+}
+
